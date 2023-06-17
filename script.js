82cb4dd0c485745bf5a96fe4cf0658c814225922
Amanda Skinner <amandamuscha@yahoo.com>
2023-06-17 10:45:58 -0400

    added js script and started playing with api

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+//Format for accessing sprite by pokedex number
+https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/transparent/{pokedex number}.png
+
+//Format for accessing raw JSON for pokemon
+https://pokeapi.co/api/v2/pokemon/{id or name}/
+
+//Format for basic pokemon info
+https://pokeapi.co/api/v2/pokemon-form/{id}/
+*/
+
+/*fetch(url, {
+    method: "GET" // giving the option to fetch;
+})
+*/
+
+let id = 001;
+
+let fetchResponse;
+
+function pokeFetch(id){
+    fetch("https://pokeapi.co/api/v2/pokemon-form/"+id)
+        .then((response) => response.json())
+        .then(data => {
+            fetchResponse = data;
+           })
+        .then(() => console.log(fetchResponse));
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    pokeFetch(001);
+});
